3377814686eb68bd6327ca34e8612ba6f7e69a86
Ekemini Felix <felixekemini9@gmail.com>
2022-01-29 20:36:00 +0100

    files

---
+++ models/amenity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""`Amenity` class that inherits from `BaseModel`
+"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Inherits from `BaseModel to create Amenity
+    Attributes:
+        name (str): to hold name
+    """
+    name = ""
